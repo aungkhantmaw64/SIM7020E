c65d392fb33407388245e4a623f65c8884e1d5d4
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-01-11 01:27:41 +0700

    Move fake time service implementation to test code

--- src/TimeService.cpp
+++ src/TimeService.cpp
@@ -7,18 +7,4 @@ unsigned long getMillis(void)
     return millis();
 }
 
-#else
-
-static unsigned long start = 0;
-static unsigned long step = 1;
-
-unsigned long getMillis(void)
-{
-    if (start < 10000UL)
-        start += step;
-    else
-        start = 0;
-    return start;
-}
-
 #endif
