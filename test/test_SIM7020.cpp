3790581220fc1837ae71711424ffc0721e0b2dca
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-01-10 16:54:10 +0700

    Remove old inefficient test code and start with new test cases

--- test/test_SIM7020.cpp
+++ test/test_SIM7020.cpp
@@ -2,155 +2,66 @@
 #include <ArduinoUnitTests.h>
 #include "SIM7020.h"
 
-#define DEFAULT_RESET_PIN 27
-#define DEFAULT_POWERKEY_PIN 26
-#define DEFAULT_RTCEINT_PIN 4
-#define DEFAULT_BAUDRATE 115200
+#define DEFAULT_RESET_PIN (int)(27)
+#define DEFAULT_POWERKEY_PIN (int)(26)
+#define DEFAULT_RTCEINT_PIN (int)(4)
+#define DEFAULT_BAUDRATE (unsigned long)(115200)
 
-static String responseBufferstorage = "";
-static SIM7020 *gmodem = NULL;
+class MockSerial : public Stream
+{
+private:
+    unsigned long baudrate;
+
+public:
+    MockSerial()
+    {
+    }
+    ~MockSerial()
+    {
+    }
+    unsigned long getBaudrate(void)
+    {
+        return baudrate;
+    }
+};
+
+static MockSerial mock_serial;
+
+static SIM7020 modem(mock_serial,
+                     DEFAULT_RESET_PIN,
+                     DEFAULT_POWERKEY_PIN,
+                     DEFAULT_RTCEINT_PIN);
+
 static GodmodeState *state = NULL;
 
 unittest_setup()
 {
     state = GODMODE();
     state->reset();
-
-    gmodem = new SIM7020(&Serial, 27, 26, 4);
 }
 
 unittest_teardown()
 {
     state->reset();
-    delete gmodem;
-    gmodem = NULL;
 }
 
-unittest(PinsAreLOW_AfterDefaultInitialization)
+unittest(BeginTurnsModemPowerOn_IgnoreRestart)
 {
-    /* Initialize mock objects */
-    state->digitalPin[DEFAULT_RESET_PIN] = HIGH;
-    state->digitalPin[DEFAULT_POWERKEY_PIN] = HIGH;
-    state->digitalPin[DEFAULT_RTCEINT_PIN] = HIGH;
+    modem.begin(false);
 
-    /* Initialize module under test */
-
-    SIM7020 *modem = new SIM7020(&Serial, 27, 26, 4);
-
-    /* Assertions */
-    assertEqual(LOW, state->digitalPin[DEFAULT_RESET_PIN]);
-    assertEqual(LOW, state->digitalPin[DEFAULT_POWERKEY_PIN]);
-    assertEqual(LOW, state->digitalPin[DEFAULT_RTCEINT_PIN]);
-
-    delete modem;
-}
-
-unittest(OnlyResetPinsStateChangeToLow_AfterMinimumInitialization)
-{
-    /* Initialize mock objects */
-    state->digitalPin[DEFAULT_RESET_PIN] = HIGH;
-    state->digitalPin[DEFAULT_POWERKEY_PIN] = HIGH;
-    state->digitalPin[DEFAULT_RTCEINT_PIN] = HIGH;
-
-    SIM7020 *modem = new SIM7020(&Serial, 27);
-
-    /* Assertions */
-    assertEqual(LOW, state->digitalPin[DEFAULT_RESET_PIN]);
-    assertEqual(HIGH, state->digitalPin[DEFAULT_POWERKEY_PIN]);
-    assertEqual(HIGH, state->digitalPin[DEFAULT_RTCEINT_PIN]);
-
-    delete modem;
-}
-
-unittest(ThisTestFails_WhenBaudrateDoesntMatchWithModem)
-{
-    gmodem->begin(115200);
-}
-
-unittest(BeginSucceeds_RestartEnabled)
-{
-    gmodem->begin(115200, true);
-
-    bool actual[4];
-    bool expected[4] = {LOW, LOW, HIGH, LOW};
-    int numMoved = state->digitalPin[DEFAULT_RESET_PIN].toArray(actual, 4);
-    assertEqual(4, numMoved);
-
-    for (int i = 0; i < 2; i++)
+    /*Text fixtures*/
+    const int powerKeyPin = 26;
+    int totalExpectedPinStateChanges = 3;
+    bool initialState = LOW;
+    bool expectedPinStates[3] = {initialState, LOW, HIGH};
+    bool actualPinStates[3];
+    int numStateChanges = state->digitalPin[powerKeyPin].toArray(actualPinStates,
+                                                                 totalExpectedPinStateChanges);
+    assertEqual(numStateChanges, totalExpectedPinStateChanges);
+    for (auto index{0}; index < totalExpectedPinStateChanges; index++)
     {
-        assertEqual(expected[i], actual[i]);
+        assertEqual(expectedPinStates[index], actualPinStates[index]);
     }
-}
-
-unittest(ATCommandSends)
-{
-    gmodem->begin(115200, true);
-    gmodem->sendATCommand("AT");
-
-    assertEqual("AT\r", state->serialPort[0].dataOut);
-}
-
-unittest(ModemReturnsCorrectReponseStatuses)
-{
-    gmodem->begin(115200, true);
-    gmodem->sendATCommand("AT\r");
-
-    /* Command Success*/
-    state->serialPort[0].dataIn = "\r\nOK\r\n";
-    String responseBufferstorage;
-    ATResponseStatus_t status = gmodem->waitForResponse(300, responseBufferstorage);
-    assertEqual(CommandSuccess, status);
-    assertEqual("\r\nOK\r\n", responseBufferstorage);
-    /* Timeout Error occurs*/
-    state->serialPort[0].dataIn = "";
-    status = gmodem->waitForResponse(300, responseBufferstorage);
-    assertEqual(TimeoutError, status);
-    assertEqual("", responseBufferstorage);
-    /* Invalid command error occurs*/
-    state->serialPort[0].dataIn = "\r\nERROR\r\n";
-    status = gmodem->waitForResponse(300, responseBufferstorage);
-    assertEqual(InvalidCommand, status);
-    assertEqual("\r\nERROR\r\n", responseBufferstorage);
-    /* Unknown error due to potential hardware issue occurs*/
-    state->serialPort[0].dataIn = "\r\n@3s#fsfs@\r\n";
-    status = gmodem->waitForResponse(300, responseBufferstorage);
-    assertEqual(UnknownStatus, status);
-    assertEqual("\r\n@3s#fsfs@\r\n", responseBufferstorage);
-}
-
-unittest(ModemIsReady)
-{
-    state->serialPort[0].dataIn = "\r\nOK\r\n";
-    assertEqual(true, gmodem->ready());
-    assertEqual("AT\r", state->serialPort[0].dataOut);
-}
-
-unittest(ModemIsNotReady)
-{
-    state->serialPort[0].dataIn = "";
-    assertEqual(false, gmodem->ready());
-    assertEqual("AT\r", state->serialPort[0].dataOut);
-}
-
-unittest(PowerOnModemByHardwarePin)
-{
-    gmodem->turnPowerOn();
-    bool actual[4];
-    bool expected[4] = {LOW, LOW, LOW, HIGH};
-    int numMoved = state->digitalPin[DEFAULT_POWERKEY_PIN].toArray(actual, 4);
-
-    assertEqual(4, numMoved);
-    for (auto i{0}; i < 4; i++)
-    {
-        assertEqual(expected[i], actual[i]);
-    }
-}
-
-unittest(GetIMEI)
-{
-    state->serialPort[0].dataIn = "\r\n123456789012345\r\n\r\nOK\r\n";
-    String imei;
-    assertEqual("123456789012345", gmodem->getIMEI());
 }
 
 unittest_main()
